e7722b7cd6b91b49d1b9fb6cf2f3912d1f4669ef
bulrfire <2592718594@qq.com>
2023-08-18 21:26:26 +0800

    更新 Demo.java

--- try/src/com/bluefire_fox/die/Demo.java
+++ try/src/com/bluefire_fox/die/Demo.java
@@ -2,10 +2,6 @@ package com.bluefire_fox.die;
 
 public class Demo {
     public static void main(String[] args) {
-        a(2);
-    }
 
-    public static void a(int a){
-        System.out.println(a);
     }
 }
