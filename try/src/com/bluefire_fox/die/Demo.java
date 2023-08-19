7d16f4009c3e63206970b9f4a784b5580f2ce91f
bulrfire <2592718594@qq.com>
2023-08-19 23:25:38 +0800

    更新 Demo.java

--- try/src/com/bluefire_fox/die/Demo.java
+++ try/src/com/bluefire_fox/die/Demo.java
@@ -1,24 +1,7 @@
 package com.bluefire_fox.die;
 
-import java.util.Timer;
-
 public class Demo {
     public static void main(String[] args) {
-        Timer t = new Timer();
-        System.out.println("1");
-        int[]a=new int[20];
-
-        for(int i=1;i<=20;i++){
-
-            a[i-1]=i;
-
-            try{
-                Thread.sleep(1000);
-            }catch(Exception e){}
-
-            System.out.println(a[i-1]);
-
-        }
-        System.out.println("2");
     }
 }
+
