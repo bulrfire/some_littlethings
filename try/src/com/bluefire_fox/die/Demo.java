6dafe6efe4c55c32a8dcddd7030a704c592a50b3
bulrfire <2592718594@qq.com>
2023-08-24 12:23:02 +0800

    Update Demo.java

--- try/src/com/bluefire_fox/die/Demo.java
+++ try/src/com/bluefire_fox/die/Demo.java
@@ -1,12 +1,8 @@
 package com.bluefire_fox.die;
 
 public class Demo {
-    public static void main(String[] args) {
-        String ZT = "sb";
-        if (ZT.equals("people")){
-            System.out.println("ZT is not a bot!!");
+        private static volatile boolean s = true;
+
+        public static void tmp(){
         }
-
-
-    }
 }
