bb6f9c274bfeb338485d0849dd59daad0c4df0d0
bulrfire <2592718594@qq.com>
2023-08-25 15:03:57 +0800

    更新 Demo.java

--- try/src/com/bluefire_fox/die/Demo.java
+++ try/src/com/bluefire_fox/die/Demo.java
@@ -1,4 +1,21 @@
 package com.bluefire_fox.die;
 
+import java.util.Random;
+
 public class Demo {
+    public static void main(String[] args) {
+        int length = 5; // 指定生成的汉字位数
+        String randomChinese = generateRandomChinese(length);
+        System.out.println(randomChinese);
+    }
+
+    public static String generateRandomChinese(int length) {
+        StringBuilder sb = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            char c = (char) (0x4e00 + random.nextInt(0x9fa5 - 0x4e00 + 1));
+            sb.append(c);
+        }
+        return sb.toString();
+    }
 }
